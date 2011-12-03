1e304a4f70053bda39e9ca099b588e3dde2be76b
Sudhir Rao <sudrao@gmail.com>
2011-12-02 22:37:42 -0800

    Convert date string to date in model.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,15 @@
 class Task < ActiveRecord::Base
   belongs_to :user
+  # We get due date as a string on create
+  attr_accessor :due_string
+  # which we convert to a date object
+  before_create :convert_due
   
   validates_presence_of :title
-  validates_presence_of :due
-  validates_format_of :due, with: /\A(\d\d-){2}\d{4}\Z/
+  validates_presence_of :due_string
+  validates_format_of :due_string, with: /\A\d{4}(-\d\d){2}\Z/
+  
+  def convert_due
+    due = Date.parse(due_string) if due_string
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 describe Task do
   before(:all) do
     @user = User.find_by_username('default')
-    @params = {title: 'Task 1', due: Date.today, complete: false}
+    @params = {title: 'Task 1', due_string: '2011-12-25', complete: false}
   end
     
   
   it "creates a valid task" do
     task = @user.tasks.new(@params)
     task.valid?.should be_true
+    # task.errors.messages.should == nil
     task.save.should be_true
   end
 end
