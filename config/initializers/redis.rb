7a3c450a5fb8e7c525efb03eedf459b8a251eef4
Sudhir Rao <sudrao@gmail.com>
2011-12-06 21:27:47 -0800

    Change redis db to 0. Heroku refused db 1.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -8,7 +8,7 @@ if vcap = ENV['VCAP_SERVICES']
   $redis = Ohm.redis
 else  
   uri = URI.parse(ENV["REDISTOGO_URL"]) # Heroku convention. See development.rb.
-  redis_conf = {:db => 1, :host => uri.host, :port => uri.port, :password => uri.password }
+  redis_conf = {:db => 0, :host => uri.host, :port => uri.port, :password => uri.password }
   Ohm.connect(redis_conf)
   $redis = Ohm.redis
   puts "Initializing redis connection. Got $redis = " + $redis.inspect
