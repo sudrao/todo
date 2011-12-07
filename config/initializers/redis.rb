e01f419ea2b9ccf50181b539fba0b99055a2ccce
Sudhir Rao <sudrao@gmail.com>
2011-12-06 16:07:54 -0800

    Attempt to get this on CloudFoundry. Works OK on local server but not yet on CloudFoundry.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,14 @@
-redis_conf = {:db => 1, :port => Rails.env.test? ? 6378 : 6379}
-Ohm.connect(redis_conf)
-$redis = Ohm.redis
-puts "Initializing redis connection. Got $redis = " + $redis.inspect
+if vcap = ENV['VCAP_SERVICES']
+  # cloudfoundry stuff
+  services = JSON.parse(vcap)
+  redis_key = services.keys.select { |svc| svc =~ /redis/i }.first
+  redis = services[redis_key].first['credentials']
+  redis_conf = {:db => 1, :host => redis['hostname'], :port => redis['port'], :password => redis['password']}
+  Ohm.connect(redis_conf)
+  $redis = Ohm.redis
+else
+  redis_conf = {:db => 1, :port => Rails.env.test? ? 6378 : 6379}
+  Ohm.connect(redis_conf)
+  $redis = Ohm.redis
+  puts "Initializing redis connection. Got $redis = " + $redis.inspect
+end
